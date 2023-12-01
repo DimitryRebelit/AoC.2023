8b2af304fa008bd864ed46730f49bc6e459c9683
Dimitry Volker <dimitry@rebelit.nl>
2023-12-01 11:59:27 +0100

    Refactored a bit for readability

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -2,6 +2,8 @@
 Console.WriteLine("Hello, World!");
 
 var puzzle1 = new Puzzle1();
+
+Console.WriteLine(puzzle1.Description);
 await puzzle1.SolvePartOneAsync();
 await puzzle1.SolvePartTwoAsync();
 Console.ReadKey();
--- AoC/Puzzles/Puzzle1.cs
+++ AoC/Puzzles/Puzzle1.cs
@@ -7,64 +7,69 @@ public class Puzzle1 : BasePuzzle, IPuzzle
 {
     /// <inheritdoc />
     public string Description => "--- Day 1: Trebuchet?! ---";
-    private readonly Regex _firstLastRegex = new (@"^\D*(\d).*\D*(\d)\D*$");
-    private readonly Regex _single = new (@"\d");
-    private readonly string _numberPattern = @"(?=(\d|one|two|three|four|five|six|seven|eight|nine))";
 
     /// <inheritdoc />
     public async Task SolvePartOneAsync()
     {
-        var result = new List<int>();
         var input = await base.ReadAllLinesFromInputAsync(1,1);
-        foreach (var line in input)
-        {
-            var matches = _firstLastRegex.Match(line);
-            if(matches.Groups.Count > 2)
-                result.Add(int.Parse($"{matches.Groups[1]}{matches.Groups[2]}"));
-            else
-            {
-                var match = _single.Match(line);
-                result.Add(int.Parse($"{match.Groups[0]}{match.Groups[0]}"));
-            }
-        }
-        Console.WriteLine($"Result: {result.Sum()}");
+        var result = input.Select(GetFirstAndLastNumber).ToList();
+        Console.WriteLine($"    Result: {result.Sum()}");
     }
 
     /// <inheritdoc />
     public async Task SolvePartTwoAsync()
     {
-        var result = new List<int>();
-
         var input = await base.ReadAllLinesFromInputAsync(1,2);
-        foreach (var line in input)
+        var result = input
+            .Select(ExtractNumbersFromString)
+            .Select(GetFirstAndLastNumber)
+            .ToList();
+
+        Console.WriteLine($"    Result: {result.Sum()}");
+    }
+
+    /// <summary>
+    ///     Replace number words with digits
+    /// </summary>
+    /// <param name="line"></param>
+    /// <remarks>
+    ///    Replaces the words one till nine with digits
+    /// </remarks>
+    /// <returns></returns>
+    private string ExtractNumbersFromString(string line)
+    {
+        var replacedLine = string.Empty;
+        var pattern = @"(?=(\d|one|two|three|four|five|six|seven|eight|nine))";
+        var numberWordMatch = Regex.Match(line, pattern);
+
+        while (numberWordMatch.Success)
         {
-            var replacedLine = "";
-            var numberWordMatch = Regex.Match(line, _numberPattern);
+            var matchedValue = numberWordMatch.Groups[1].Value;
+            var number = char.IsLetter(matchedValue[0]) ?
+                (Array.IndexOf(new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }, matchedValue) + 1).ToString() :
+                matchedValue;
 
-            while (numberWordMatch.Success)
-            {
-                var matchedValue = numberWordMatch.Groups[1].Value;
-                var number = char.IsLetter(matchedValue[0]) ?
-                    (Array.IndexOf(new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }, matchedValue) + 1).ToString() :
-                    matchedValue;
-
-                replacedLine += number;
-                numberWordMatch = numberWordMatch.NextMatch();
-            }
-
-            var matches = _firstLastRegex.Match(replacedLine);
-            if (matches.Groups.Count > 2)
-            {
-                result.Add(int.Parse($"{matches.Groups[1]}{matches.Groups[2]}"));
-            }
-            else
-            {
-                var match = _single.Match(replacedLine);
-                Console.WriteLine($"{match.Groups[0]}{match.Groups[0]}");
-                result.Add(int.Parse($"{match.Groups[0]}{match.Groups[0]}"));
-            }
+            replacedLine += number;
+            numberWordMatch = numberWordMatch.NextMatch();
         }
 
-        Console.WriteLine($"Result: {result.Sum()}");
+        return replacedLine;
+    }
+
+    /// <summary>
+    ///     Get the first and last number from a string
+    /// </summary>
+    /// <param name="line"></param>
+    /// <returns></returns>
+    private int GetFirstAndLastNumber(string line)
+    {
+        Regex pattern = new (@"^\D*(\d).*\D*(\d)\D*$");
+        var match = pattern.Match(line);
+        if (match.Groups.Count > 2)
+            return int.Parse($"{match.Groups[1]}{match.Groups[2]}");
+
+        pattern = new (@"\d");
+        match = pattern.Match(line);
+        return int.Parse($"{match.Groups[0]}{match.Groups[0]}");
     }
 }
